c64f2f95b6f18d2c7473b4d7c875097458b856f7
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-12-11 21:30:07 +0600

    pdf added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import profile from '../../../assets/bannerImg/profile.jpg'
+import Resume from '../../../assets/resume/Rashel Hossain-Resume2.pdf'
 
 const Banner = () => {
     return (
@@ -12,7 +13,11 @@ const Banner = () => {
                             Hello,  <br /> I'm Rashel Hoosain
                         </h2>
                         <p className='text-xl text-black'>There are many variations of available, We provide <br /> The best service have setisfied products in our customer.</p>
-                        <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white mt-2">Get Explore</button>
+                        <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white mt-2">
+                            <a href={Resume}>Download Resume</a>
+                        </button>
+
+                        {/* <Link className='btn' src="/resume/Rashel Hossain-Resume2.pdf">Download Resume</Link> */}
                     </div>
                 </div>
             </div>
